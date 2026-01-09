356d779003d8fb0d9cc705c3397346de88e5b547
Prashant <dev.prashantroshan@gmail.com>
2026-01-09 16:48:24 +0530

    New commit

--- code-with-harry/src/components/primary/Navbar.js
+++ code-with-harry/src/components/primary/Navbar.js
@@ -90,12 +90,6 @@ export default function Navbar(props) {
               </label>
               <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" checked={props.theme === 'dark'} onChange={() => props.toggleMode()} />
             </div>
-            {/* <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form> */}
           </div>
         </div>
       </nav>
